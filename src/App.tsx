c7aac486d2e1ddb50c62d0b7e918d57be0398c60
Petr Kočí <petr.koci@samizdat.cz>
2024-05-16 19:01:35 +0200

    filter data

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import './App.css'
 
 import strany from "./assets/data/strany.json"
 
-console.log(strany)
+type View = {
+  otazka: number,
+  strany: number[]
+}
+
+type Odpoved = string[]
 
 function App() {
   const [otazky, setOtazky] = useState(0)
-  const [odpovedi, setOdpovedi] = useState(0)
+  const [odpovedi, setOdpovedi] = useState<Odpoved[]>([])
   const [kandidati, setKandidati] = useState(0)
-  const [view, setView] = useState({})
-  const [selected, setSelected] = useState([])
+  const [view, setView] = useState<View>({ otazka: 0, strany: [] })
+  const [selected, setSelected] = useState<Odpoved[]>([])
 
+  //load data, the set initial view
   useEffect(() => {
     Promise.all([
       fetch('https://worker-muddy-disk-d6e4.jan-cibulka-ee5.workers.dev/?sheet=otazky')
@@ -26,18 +32,25 @@ function App() {
         .then((response) => response.json())
         .then((data) => setKandidati(data.values))
     ]).then(() => {
-      setView({ otazka: 0, strany: Array.from({ length: 30 }, (_, i) => i) });
+      setView({ otazka: 1, strany: Array.from({ length: 30 }, (_, i) => i) });
     });
   }, [])
 
+  //prepare data for the view
   useEffect(() => {
+    // filter odpovedi for the otazka and all the strany in current view
+    const odpovediToShow = odpovedi.filter((odpoved) => {
+      return parseInt(odpoved[3]) === view.otazka && view.strany.includes(parseInt(odpoved[2]));
+    });
 
+    setSelected(odpovediToShow);
   }, [view])
 
   return (
-    <>
-      <h1 className="text-xl font-bold">Čus bus</h1>
-    </>
+    <div className="flex flex-wrap justify-center">
+      <div className="w-60 h-80 bg-zinc-200">
+      </div>
+    </div>
   )
 }
 
