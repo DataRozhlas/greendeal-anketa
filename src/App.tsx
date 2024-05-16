bd0f4d82b4ede73cb99d8ebb57ec9734686ec479
Petr Kočí <petr.koci@samizdat.cz>
2024-05-16 18:42:49 +0200

    load data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+import strany from "./assets/data/strany.json"
+
+console.log(strany)
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [otazky, setOtazky] = useState(0)
+  const [odpovedi, setOdpovedi] = useState(0)
+  const [kandidati, setKandidati] = useState(0)
+  const [view, setView] = useState({})
+  const [selected, setSelected] = useState([])
+
+  useEffect(() => {
+    Promise.all([
+      fetch('https://worker-muddy-disk-d6e4.jan-cibulka-ee5.workers.dev/?sheet=otazky')
+        .then((response) => response.json())
+        .then((data) => setOtazky(data.values)),
+
+      fetch('https://worker-muddy-disk-d6e4.jan-cibulka-ee5.workers.dev/?sheet=odpovedi')
+        .then((response) => response.json())
+        .then((data) => setOdpovedi(data.values)),
+
+      fetch('https://worker-muddy-disk-d6e4.jan-cibulka-ee5.workers.dev/?sheet=kandidati')
+        .then((response) => response.json())
+        .then((data) => setKandidati(data.values))
+    ]).then(() => {
+      setView({ otazka: 0, strany: Array.from({ length: 30 }, (_, i) => i) });
+    });
+  }, [])
+
+  useEffect(() => {
+
+  }, [view])
 
   return (
     <>
