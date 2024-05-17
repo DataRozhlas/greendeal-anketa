2e6874d37d09d2ccb70b6956654c6266545c5512
Petr Kočí <petr.koci@samizdat.cz>
2024-05-17 06:55:19 +0200

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 
 import strany from "./assets/data/strany.json"
 
+import { CircleUser } from 'lucide-react';
+import { Badge } from "@/components/ui/badge"
+
+
 type View = {
   otazka: number,
   strany: number[]
@@ -43,14 +47,39 @@ function App() {
       return parseInt(odpoved[3]) === view.otazka && view.strany.includes(parseInt(odpoved[2]));
     });
 
+    odpovediToShow.sort((a, b) => parseInt(a[2]) - parseInt(b[2]))
+
     setSelected(odpovediToShow);
   }, [view])
 
+  //get party name
+  const getPartyName = (record: string[], format: string, strany: any[]) => {
+    return strany.find((strana: { ESTRANA: string }) => parseInt(strana.ESTRANA) === parseInt(record[2]))[format]
+  }
+
   return (
-    <div className="flex flex-wrap justify-center">
-      <div className="w-60 h-80 bg-zinc-200">
-      </div>
+    <div className="flex flex-wrap justify-center max-w-[1070px] mx-auto">
+      {selected.length > 0 && selected.map(respondent => {
+        return (
+          <div key={crypto.randomUUID()} className="w-1/2 sm:w-1/3 lg:w-1/4 p-2">
+            <div className="bg-white shadow-md rounded px-4 pt-2 pb-4 mb-4">
+              <div className="flex justify-between items-start">
+                <h2 className="font-bold">{getPartyName(respondent, "ZKRATKAE30", strany)}</h2>
+                <Badge variant={respondent[4] === "ANO" ? "default" : respondent[4] === "NE" ? "destructive" : "outline"}>{respondent[4]}</Badge>
+              </div>
+              <div className="flex gap-2 items-center my-2">
+                <CircleUser size={48} color='rgb(161 161 170)' />
+                <p className="text-zinc-600 text-sm">{respondent[0]}</p>
+              </div>
+              <div>
+                <p className="text-sm">{respondent[5]}</p>
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </div>
+
   )
 }
 
